3222d7a44cfdc5b798b9a11a2e9271d008137618
Ryan Wang <rywang@outlook.com>
2016-07-10 15:56:54 -0700

    Clean up interaction
    - Pull out `simpleInteract`
    - Flush stdout after setting prompt

--- commandLine.hs
+++ commandLine.hs
@@ -1,12 +1,18 @@
 import qualified System.IO
 import qualified Data.Logic.Propositional as Prop
 
+-- Account for lazy evaluation of input. This allows one to process a whole
+-- line using `f`, as in an imperative language.
+simpleInteract :: (String -> String) -> IO ()
+simpleInteract f = System.IO.interact (unlines . map f . lines)
+
 stringAsExpression :: String -> String
 stringAsExpression input = case Prop.parseExpr "" input of
-                             Left err -> "Failed to parse " ++ show err
-                             Right expr -> show expr
+                       Left err -> "Failed to parse " ++ show err
+                       Right expr -> "Parsed expression: " ++ show expr
 
 main :: IO ()
 main = do
-  System.IO.putStr "Please enter a proposition: "
-  System.IO.interact (unlines . map stringAsExpression . lines)
+  System.IO.putStr "Please input a proposition: "
+  System.IO.hFlush System.IO.stdout
+  simpleInteract stringAsExpression
