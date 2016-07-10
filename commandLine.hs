4c07b2f57173b5985d124f67116018f57e896a17
Ryan Wang <rywang@outlook.com>
2016-07-09 23:58:29 -0700

    WIP Command line interface

---
+++ commandLine.hs
@@ -0,0 +1,12 @@
+import qualified System.IO
+import qualified Data.Logic.Propositional as Prop
+
+stringAsExpression :: String -> String
+stringAsExpression input = case Prop.parseExpr "" input of
+                             Left err -> "Failed to parse " ++ show err
+                             Right expr -> show expr
+
+main :: IO ()
+main = do
+  System.IO.putStr "Please enter a proposition: "
+  System.IO.interact stringAsExpression
