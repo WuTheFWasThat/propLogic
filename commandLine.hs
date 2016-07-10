fed042e748c3220f382d5182196a6022921c704b
Ryan Wang <rywang@outlook.com>
2016-07-10 16:20:45 -0700

    Cause the interaction to loop

--- commandLine.hs
+++ commandLine.hs
@@ -1,18 +1,23 @@
 import qualified System.IO
 import qualified Data.Logic.Propositional as Prop
 
--- Account for lazy evaluation of input. This allows one to process a whole
--- line using `f`, as in an imperative language.
-simpleInteract :: (String -> String) -> IO ()
-simpleInteract f = System.IO.interact (unlines . map f . lines)
+loopingInteraction :: String -> (String -> String) -> IO ()
+loopingInteraction prompt interaction = do
+  System.IO.putStr prompt
+  System.IO.hFlush System.IO.stdout
+  line <- System.IO.getLine
+  if null line
+    then return ()
+    else do
+      System.IO.putStrLn $ interaction line
+      loopingInteraction prompt interaction
 
-stringAsExpression :: String -> String
-stringAsExpression input = case Prop.parseExpr "" input of
+displayExpression :: String -> String
+displayExpression input = case Prop.parseExpr "" input of
                        Left err -> "Failed to parse " ++ show err
                        Right expr -> "Parsed expression: " ++ show expr
 
 main :: IO ()
 main = do
-  System.IO.putStr "Please input a proposition: "
-  System.IO.hFlush System.IO.stdout
-  simpleInteract stringAsExpression
+  let prompt = "Please input a proposition: "
+  loopingInteraction prompt displayExpression
